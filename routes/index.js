9497f77b91dd9779d8ac00925bde647f9ad022b1
John <john@homeselect.com>
2018-03-09 10:10:45 +0100

    basic form submit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Firma' });
+});
+
+router.post('/firma', function (req, res) {
+  console.log(req.body.name);
+  console.log(req.body.job);
+  var name = req.body.name
+  var job = req.body.job
+  res.render('firma', { name: name, job: job});
 });
 
 module.exports = router;
