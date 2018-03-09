e2e7559cb73e4ed5c8c46100e50a51332a834ac0
John <john@homeselect.com>
2018-03-09 17:04:09 +0100

    format photo name from person name

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,24 @@ var express = require('express');
 var router = express.Router();
 const jade = require("jade");
 
+var slug = function(str) {
+  str = str.replace(/^\s+|\s+$/g, ''); // trim
+  str = str.toLowerCase();
+
+  // remove accents, swap ñ for n, etc
+  var from = "ãàáäâẽèéëêìíïîõòóöôùúüûñç·/_,:;";
+  var to   = "aaaaaeeeeeiiiiooooouuuunc------";
+  for (var i=0, l=from.length ; i<l ; i++) {
+    str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+  }
+
+  str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+    .replace(/\s+/g, '_') // collapse whitespace and replace by -
+    .replace(/-+/g, '_'); // collapse dashes
+
+  return str;
+};
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Nueva Firma' });
@@ -21,7 +39,7 @@ router.post('/show', function (req, res) {
   { 
     title: locals.company + " - " + locals.first_name,
     company: locals.company,
-    photo: locals.first_name + "_" + locals.last_name + ".jpg",
+    photo: slug(locals.first_name + " " + locals.last_name) + ".jpg",
     first_name: locals.first_name, 
     last_name: locals.last_name, 
     position: locals.position,
